c35b27b4cea4688bfee1105206bd7a6afbd21b11
jayiuk <jayiuk987@gmail.com>
2024-06-26 10:44:07 +0900

    그리드서치

--- break_prediction.py
+++ break_prediction.py
@@ -1,10 +1,19 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+
 r_data = pd.read_csv('./km_break.csv')
 data = r_data.drop(labels = ['고장구분', 'pca_x', 'pca_y'], axis = 1)
 x_data = data.iloc[:, 0:3]
 y_data = data.iloc[:, -1]
 train_x, test_x, train_y, test_y = train_test_split(x_data, y_data, test_size = 0.2, random_state = 42)
 
+params = {'n_estimators' : [10, 500],
+          'max_depth' : [1, 100],
+          'random_state' : [20, 70],
+          'criterion' : ["gini", "entropy", "log_loss"]}
+
 rf = RandomForestClassifier()
+gv = GridSearchCV(estimator = rf, param_grid = params, scoring = 'accuracy')
+gv.fit(train_x, train_y)
+print(gv.best_params_)
