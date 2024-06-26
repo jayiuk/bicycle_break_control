888500bd989a8202a800796536a58df09d62b1e3
jayiuk <jayiuk987@gmail.com>
2024-06-26 11:32:05 +0900

    랜덤포레스트 정확도 0.98

--- break_prediction.py
+++ break_prediction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 r_data = pd.read_csv('./km_break.csv')
 data = r_data.drop(labels = ['고장구분', 'pca_x', 'pca_y'], axis = 1)
@@ -14,6 +15,22 @@ params = {'n_estimators' : [10, 500],
           'criterion' : ["gini", "entropy", "log_loss"]}
 
 rf = RandomForestClassifier()
-gv = GridSearchCV(estimator = rf, param_grid = params, scoring = 'accuracy')
+gv = GridSearchCV(estimator = rf, param_grid = params, scoring = 'accuracy', refit = True)
 gv.fit(train_x, train_y)
 print(gv.best_params_)
+rf_clf = RandomForestClassifier(n_estimators = 10, max_depth = 100, criterion = 'gini', random_state = 20)
+def train(model, train, target):
+    model = model
+    train = train
+    target = target
+
+    result = model.fit(train, target)
+    return result
+
+def predict(model, test):
+    pred = model.predict(test)
+    return pred
+
+
+accuracy = accuracy_score(test_y, predict(train(rf_clf, train_x, train_y), test_x))
+print(accuracy)
